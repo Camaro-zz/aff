f6fec8f2be3723c5bb6ce864699ab840f8fbf6bc
吴鑫 <wuxin@mail.weiba66.com>
2016-07-27 11:31:06 +0800

    啊啊

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,53 @@ var uglify = require('gulp-uglify');
 var minifyHTML = require('gulp-minify-html');
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.extend('scss', function() {
+    gulp.task('scss', function() {
+        gulp.src('public/assets/sass/app.scss')
+            .pipe(sass())
+            .pipe(minifyCss())
+            .pipe(gulp.dest('./public/css'));
+    });
+    return this.queueTask('scss');
+});
+
+
+elixir.extend('js', function() {
+    gulp.task('js', function() {
+        gulp.src('public/assets/js/libs/require.js')
+            .pipe(uglify())
+            .pipe(gulp.dest("public/js"));
+
+        gulp.src('public/assets/js/core/global.js')
+            .pipe(requirejsOptimize({
+                name: 'core/global',
+                mainConfigFile: "public/assets/js/config.js",
+                baseUrl: 'public/assets/js',
+                exclude: ['exclude']
+            }))
+            .pipe(gulp.dest("public/js"));
+
+        gulp.src('public/assets/js/core/libs.js')
+            .pipe(requirejsOptimize({
+                name: 'core/libs',
+                mainConfigFile: "public/assets/js/config.js",
+                baseUrl: 'public/assets/js',
+                exclude: ['core/global', 'exclude']
+            }))
+            .pipe(gulp.dest("public/js"));
+
+        gulp.src('public/assets/js/bootstrap.js')
+            .pipe(requirejsOptimize({
+                name: 'bootstrap',
+                optimize: 'none',
+                mainConfigFile: "public/assets/js/config.js",
+                baseUrl: 'public/assets/js',
+                exclude: ['core/global', 'core/libs', 'exclude']
+            }))
+            .pipe(gulp.dest("public/js"));
+    });
+    return this.queueTask('js');
+});
 
 elixir(function(mix) {
     mix.sass('app.scss');
